685e82e4133116a15befa2d84b94cd3e7384e7d1
evamonika <evamonika53p@gmail.com>
2022-01-04 05:38:34 +0700

    REST API Authorization

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//API Route untuk register
+Route::post('/register', [AuthController::class, 'register']);
+//API Route untuk login
+Route::post('/login', [AuthController::class, 'login']);
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    Route::resource('programs', ProgramController::class);
+    
+    //API Route untuk logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
